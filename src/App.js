f54849d4b2a90fbcbffdd605457e66673137d8d8
CodeWithNaveed <getinfo.naveed@gmail.com>
2024-09-21 02:12:37 +0500

    completed

--- src/App.js
+++ src/App.js
@@ -10,50 +10,69 @@ function App() {
   const [num, setNum] = useState(0);
   const [quotation, setQuotation] = useState(<h1>Best Three Success Rules</h1>);
 
+
   const decrement = () => {
-    if (num > 1) {
+    if (num > 0) {
       setNum(num - 1);
     }
   };
 
   const increment = () => {
-    if (num < 3) { 
+    if (num < 3) {
       setNum(num + 1);
     }
   };
- 
+
+  const [classname,setclassname] = useState("");
+
+  const RuleZero = () => {
+    setQuotation(<h1>Best Three Success Rules</h1>);
+  }
   const RuleOne = () => {
+    setclassname("action")
     setQuotation(<h3>Success is the sum of small efforts, repeated day in and day out.</h3>);
+    setNum(1);
   }
   const RuleTwo = () => {
+    setclassname("action")
     setQuotation(<h3>The best way to predict the future is to create it.</h3>);
+    setNum(2);
   }
   const RuleThree = () => {
+    setclassname("action")
     setQuotation(<h3>It does not matter how slowly you go as long as you do not stop.</h3>);
+    setNum(3);
   }
 
-  
+  const [zerorule, setzerorule] = useState();
+  const [onerule, setonerule] = useState();
+  const [tworule, settworule] = useState();
+  const [threerule, setthreerule] = useState();
+
 
   useEffect(() => {
+    if (num === 0) {
+      setzerorule(RuleZero);
+    }
     if (num === 1) {
-      setQuotation(<h3>Success is the sum of small efforts, repeated day in and day out.</h3>);
+      setonerule(RuleOne);
     }
     if (num === 2) {
-      setQuotation(<h3>The best way to predict the future is to create it.</h3>);
+      settworule(RuleTwo);
     }
     if (num === 3) {
-      setQuotation(<h3>It does not matter how slowly you go as long as you do not stop.</h3>);
+      setthreerule(RuleThree);
     }
   }, [num]);
-  
-  const arry = [RuleOne, RuleTwo, RuleThree];
+
+  const arry = [RuleZero, RuleOne, RuleTwo, RuleThree];
 
   return (
     <div style={{ textAlign: "center", width: "50%", height: "80vh", backgroundColor: "lightgrey" }}>
       <div style={{ display: "flex", justifyContent: "space-around" }}>
-        <Button title={1} onClick={arry[0]} num={num}/>
-        <Button title={2} onClick={arry[1]} num={num}/>
-        <Button title={3} onClick={arry[2]} num={num}/>
+        <Button title={1} classon={classname} onClick={arry[1]} num={num} />
+        <Button title={2} classon={classname} onClick={arry[2]} num={num} />
+        <Button title={3} classon={classname} onClick={arry[3]} num={num} />
       </div>
 
       <div className='quotation-div'>
@@ -61,17 +80,18 @@ function App() {
       </div>
 
       <div style={{ display: "flex", justifyContent: "space-around" }}>
-        <Button title="Previous" onClick={decrement} />
-        <Button title="Next" onClick={increment} />
+        <Button title="Previous" onClick={decrement}/>
+        <Button title="Next" onClick={increment}/>
       </div>
     </div>
   );
 };
 
-const Button = ({title, onClick, num}) => {
+const Button = ({ title, onClick, num, classon}) => {
+
   return (
-    <button onClick={onClick} style={{ cursor: "pointer", backgroundColor: title === num ? "green" : ""}}>
-      {title}
+    <button className={classon} onClick={onClick} style={{backgroundColor: title === num ? "red" : "" }}>
+      {title} 
     </button>
   );
 };
